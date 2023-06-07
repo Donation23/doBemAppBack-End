d9392a17d1bf0aff8cf9ba318210a38571a6efb6
Flamerson De Andrade <68919243+Flamerson@users.noreply.github.com>
2023-06-06 21:33:47 -0300

    Update index.js
    json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello, world!");
+    res.json({"res": "Hello, world!"});
 })
 
 app.listen(PORT, console.log(`Server start by router ${PORT}`));
