8cb177c64ef945e8ebce7815497d5ad49be0c087
Flamerson De Andrade <68919243+Flamerson@users.noreply.github.com>
2023-06-06 21:30:37 -0300

    Update index.js
    frufru

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const app = express();
+
+const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello, world!");
 })
 
-app.listen(3000);
+app.listen(PORT, `Server start by router ${PORT}`);
