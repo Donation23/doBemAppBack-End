24dcac72cab084e7afda659854d00c3ec620ddbe
flamerson.andrade <flamerson.andrade@esig.com.br>
2023-07-22 13:31:10 -0300

    Change to TypeScript

---
+++ src/app/controller/FirstController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+class FirstController{
+
+  public home(req:Request, res:Response) {
+    return res.json({
+      response: 'Hello World'
+    });
+  }
+
+}
+
+export const firstController = new FirstController();
